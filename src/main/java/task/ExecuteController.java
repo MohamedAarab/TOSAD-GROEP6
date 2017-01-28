d707f305b3d43ce033f0a3c5e72c689be9b6f2d4
lb321 <lbos321@gmail.com>
2017-01-28 20:36:17 +0100

    Added RestService to execute code

--- src/main/java/task/ExecuteController.java
+++ src/main/java/task/ExecuteController.java
@@ -3,13 +3,26 @@ package task;
 import domain.targetDatabase.ITargetDatabaseService;
 import domain.targetDatabase.TargetDatabaseServiceImp;
 
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
 /**
  * Created by lucas on 27-1-2017.
  */
+@Path("/execute")
 public class ExecuteController implements IExecuteController {
+    @Path("/{host}/{triggerCode}")
+    @GET
+    @Produces("application/json")
     @Override
-    public String executeScript(String host, String triggerCode) {
+    public String executeScript(@PathParam("host") String host, @PathParam("triggerCode") String triggerCode) {
         ITargetDatabaseService targetDatabaseService = TargetDatabaseServiceImp.getInstance();
-        return targetDatabaseService.executeScript(host, triggerCode);
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        job.add("message", targetDatabaseService.executeScript(host, triggerCode));
+        return job.build().toString();
     }
 }
